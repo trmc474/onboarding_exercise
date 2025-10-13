4822e1230543efaeeae85bacc73b92af28b38347
trmc <trmc@netcompany.com>
2025-10-13 16:24:38 +0700

    chore: initialize project

---
+++ src/main/java/com/netcompany/onboarding_exercise/OnboardingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.netcompany.onboarding_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netcompany/onboarding_exercise/OnboardingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcompany.onboarding_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
